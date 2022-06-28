0e21280a9c648eadcdb8d2a9f333b8f63cb0c2c1
Karthick Sundararajan <sky.karthick@gmail.com>
2022-06-28 23:20:05 +0200

    - Selection Sort - Bubble Sort - Insertion Sort

--- src/DsAlgo/Program.cs
+++ src/DsAlgo/Program.cs
@@ -2,17 +2,48 @@
 
 using DsAlgo.Sorting;
 
-Console.WriteLine("Selection Sort");
-var array = new int[] { 58, 36, 72, 22, 19, 12 };
-Console.WriteLine("Before Sort");
-PrintOutput(array);
-array.SelectionSort();
-Console.WriteLine("After Sort");
-PrintOutput(array);
+ExecuteSelectionSort();
+Console.WriteLine("------------------------------------------------------------------");
+ExecuteBubbleSort();
+Console.WriteLine("------------------------------------------------------------------");
+ExecuteInsertionSort();
 Console.ReadLine();
 
 static void PrintOutput(int[] array)
 {
     var op = $"{{ {string.Join(",", array)} }}";
     Console.WriteLine(op);
+}
+
+static void ExecuteSelectionSort()
+{
+    Console.WriteLine("Selection Sort");
+    var array = new int[] { 58, 36, 72, 22, 19, 12 };
+    Console.WriteLine("Before Sort");
+    PrintOutput(array);
+    array.SelectionSort();
+    Console.WriteLine("After Sort");
+    PrintOutput(array);
+}
+
+static void ExecuteBubbleSort()
+{
+    Console.WriteLine("Bubble Sort");
+    var array = new int[] { 89, 98, 76, 28, 15, 11 };
+    Console.WriteLine("Before Sort");
+    PrintOutput(array);
+    array.BubbleSort();
+    Console.WriteLine("After Sort");
+    PrintOutput(array);
+}
+
+static void ExecuteInsertionSort()
+{
+    Console.WriteLine("Bubble Sort");
+    var array = new int[] { 4, 3, 2, 10, 12, 1, 5, 6 };
+    Console.WriteLine("Before Sort");
+    PrintOutput(array);
+    array.InsertionSort();
+    Console.WriteLine("After Sort");
+    PrintOutput(array);
 }
---
+++ src/DsAlgo/Sorting/SortExtensions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsAlgo.Sorting
+{
+    public static class SortExtensions
+    {
+        public static void SelectionSort(this int[] array)
+        {
+            if (array == null) throw new ArgumentNullException("array");
+            var arrayLength = array.Length;
+            for (int i = 0; i < arrayLength - 1; i++)
+            {
+                var index_min = i;
+
+                for (int j = i + 1; j < arrayLength; j++)
+                {
+                    if (array[j] < array[index_min])
+                    {
+                        index_min = j;
+                    }
+                }
+
+                //Swap
+                var temp = array[i];
+                array[i] = array[index_min];
+                array[index_min] = temp;
+            }
+        }
+
+        public static void BubbleSort(this int[] array)
+        {
+            var arrayLength = array.Length;
+            for (int i = 0; i < arrayLength - 1; i++)
+            {
+                for (int j = 0; j < arrayLength - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        //Swap
+                        var temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        public static void InsertionSort(this int[] array)
+        {
+            var arrayLength = array.Length;
+            for (int i = 1; i < arrayLength; i++)
+            {
+                var key = array[i];
+                var j = i - 1;
+                while (j >= 0 && array[j] > key)
+                {
+                    //shift
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = key;
+            }
+        }
+    }
+}
