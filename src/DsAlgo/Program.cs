1a3959af02861810b61ccbfc8f6112de1a0ed9e3
Karthick Sundararajan <sky.karthick@gmail.com>
2022-06-28 16:51:53 +0200

    SelectionSort

---
+++ src/DsAlgo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DsAlgo.Sorting;
+
+Console.WriteLine("Selection Sort");
+var array = new int[] { 58, 36, 72, 22, 19, 12 };
+Console.WriteLine("Before Sort");
+PrintOutput(array);
+array.SelectionSort();
+Console.WriteLine("After Sort");
+PrintOutput(array);
+Console.ReadLine();
+
+static void PrintOutput(int[] array)
+{
+    var op = $"{{ {string.Join(",", array)} }}";
+    Console.WriteLine(op);
+}
---
+++ src/DsAlgo/Sorting/SelectionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsAlgo.Sorting
+{
+    public static class SelectionSortExtension
+    {
+        public static void SelectionSort(this int[] array)
+        {
+            if(array == null) throw new ArgumentNullException("array");
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                var index_min = i;
+
+                for(int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[index_min])
+                    {
+                        index_min = j;
+                    }
+                }
+
+                //Swap
+                var temp = array[i];
+                array[i] = array[index_min];
+                array[index_min] = temp;
+            }
+        }
+    }
+}
